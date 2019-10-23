a72d77c238f61c751bf77e58e1b5f852ad8d63fd
Josh <joshgallagher24@gmail.com>
2019-10-23 19:32:47 +0100

    Set up core packages and started implementing core routes

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hello, world!'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+const url = require('url')
+const csrf = require('csurf')
+const csrfProtection = csrf({ cookie: true })
+
+app.get('/', csrfProtection, (req, res) => {
+    let query = url.parse(req.url, true).query
+    let challenge = query.login_challenge;
+
+    res.render('login', {
+        csrfToken: req.csrfToken(),
+        challenge: challenge,
+    })
+})
 
 app.listen(3000, () => console.log('Login Provider is listening!'))
